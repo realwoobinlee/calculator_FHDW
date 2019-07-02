f620b71f51701275181191435951d1adda8e2788
Daniel Hermsen <Daniel.Hermsen@itergo.com>
2019-07-02 11:20:26 +0200

    Added Tilecolormanager

---
+++ app/src/main/java/de/fhdw/calculator/Tilecolormanager.java
@@ -0,0 +1,69 @@
+package de.fhdw.calculator;
+
+
+public class Tilecolormanager {
+
+    public static int returncolor(String usage){
+        boolean numeric = true;
+
+        /**
+        *https://www.programiz.com/java-programming/examples/check-string-numeric
+         */
+        try {
+            Double num = Double.parseDouble(usage);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if (usage == "/" ||
+                usage =="*"||
+                usage =="-"||
+                usage =="+"||
+                usage =="="||
+                usage =="sqr()"||
+                usage =="vector()"||
+                usage =="prime()"||
+                usage =="ln()"||
+                usage =="xsqr()"||
+                usage =="sin()"||
+                usage =="cos()"||
+                usage =="tan()"||
+
+                usage =="log()"||
+                usage =="log10()"||
+                usage =="Summe()"||
+                usage =="seq()"||
+                usage =="summenprodukt()"||
+                usage =="bin()"
+        ){
+            return R.color.operator;
+        }
+        if (numeric == true ||
+                usage == ","||
+                usage == "^"||
+                usage == "("||
+                usage == ")"||
+                usage == "i"||
+                usage == "e"||
+                usage == "c"||
+                usage == "pi"||
+                usage == "y"||
+                usage == "x"||
+                usage == "electronm"||
+                usage == "electronc"||
+                usage == "") {
+            return R.color.constant;
+        }
+        if (usage =="view"){
+            return R.color.view;
+        }
+        if(usage =="AC"||usage=="+/-"||usage=="%"){
+           return R.color.specialoperator;
+        }
+        else {
+            return R.color.error;
+        }
+
+
+    }
+}
