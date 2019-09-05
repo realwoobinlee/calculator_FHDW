4b845a2e899562e0a1e7b254d8126ecd59129549
realwoobinlee <realwoobinlee@gmail.com>
2019-09-05 20:34:10 +0200

    zwei Buttons jeweils für Reset und startActivity von History - Woobin Lee

--- app/src/main/java/de/fhdw/hackstreetboys/calculator/History.java
+++ app/src/main/java/de/fhdw/hackstreetboys/calculator/History.java
@@ -8,7 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,11 +22,12 @@ public class History extends ListActivity {
 
     Button mMainActivityButton; // redirect to MainActivity
 
+    ListView listView;
+
     DBCalculator dB = new DBCalculator(this);;
     List<String[]> myList = new ArrayList<String[]>();
     Integer myListSize;
-    String[] eintrag = {};
-
+    String[] eintrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +35,16 @@ public class History extends ListActivity {
         setContentView(R.layout.activity_history);
         System.out.println("Start ---- ");
         dB.RetrieveHistoryData(myList);
-
+        eintrag = new String[myList.size() + 1];
+        listView=(ListView)findViewById(android.R.id.list);
         for (int i=0; i<myList.size(); i++){
            eintrag[i]= myList.get(i)[0] + " = " + myList.get(i)[1];
+           System.out.println(eintrag[i]);
         }
-        //Listview adapter
-        setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.activity_list_item,,eintrag));
 
+        //Listview adapter
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,eintrag);
+        listView.setAdapter(arrayAdapter);
 
         mMainActivityButton = (Button) findViewById(R.id.mainactivity);
         mMainActivityButton.setOnClickListener(new View.OnClickListener() {
