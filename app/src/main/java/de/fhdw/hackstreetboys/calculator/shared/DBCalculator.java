b3f013f235f2f1a7e052e598a83a24bf15193489
Dreadlocksystem <36694981+Dreadlocksystem@users.noreply.github.com>
2019-09-05 11:08:24 +0200

    Update DBCalculator.java

--- app/src/main/java/de/fhdw/hackstreetboys/calculator/shared/DBCalculator.java
+++ app/src/main/java/de/fhdw/hackstreetboys/calculator/shared/DBCalculator.java
@@ -21,9 +21,10 @@ public class DBCalculator extends SQLiteOpenHelper {
 
 
     //sql statements to create tables in the database
-    static final String DATABASE_LAYOUT_CREATE = "create table if not exists layout(position String primary key , " +
-            "width text, height text, value text); ";
-    static final String DATABASE_HISTORY_CREATE = "create table if not exists history( term text, result text); ";
+    static final String DATABASE_LAYOUT_CREATE = "create table if not exists layout" +
+            "(position String primary key , width text, height text, value text); ";
+    static final String DATABASE_HISTORY_CREATE = "create table if not exists history" +
+            "( term text, result text); ";
 
     public DBCalculator (Context context){
         super(context, LAYOUT_TABLE_NAME, null, DATABASE_VERSION);
@@ -60,8 +61,8 @@ public class DBCalculator extends SQLiteOpenHelper {
         for (int i=0; i < layoutdata.size(); i++){
             String[] tempData = layoutdata.get(i);
             try {
-                String sqlStatement = "Insert into " + LAYOUT_TABLE_NAME + " values('" + i +"','"
-                        + tempData[0] + "','" + tempData[1] + "','" + tempData[2] + "')";
+                String sqlStatement = "Insert into " + LAYOUT_TABLE_NAME + " values('" + i +"','" +
+                         tempData[0] + "','" + tempData[1] + "','" + tempData[2] + "')";
                 db.execSQL(sqlStatement);
             }catch(Exception ex) {
                 System.out.println("Exceptions " +ex);
@@ -100,7 +101,8 @@ public class DBCalculator extends SQLiteOpenHelper {
         }
 
         SQLiteDatabase db = this.getWritableDatabase();
-        String sqlStatementHistory = "insert into " + HISTORY_TABLE_NAME + " values('" + historyString + "','" + resultData + "')";
+        String sqlStatementHistory = "insert into " + HISTORY_TABLE_NAME + " values('"
+                + historyString + "','" + resultData + "')";
         db.execSQL(sqlStatementHistory);
 
         db.close();
@@ -113,7 +115,7 @@ public class DBCalculator extends SQLiteOpenHelper {
         Cursor cursor=db.rawQuery("Select *  from " + HISTORY_TABLE_NAME, null);
         if (cursor.getCount() > 0) {
             while(cursor.moveToNext()) {
-                String[] tempElem = {cursor.getString(0),cursor.getString(1)}; //Keine Pos an Stelle 0
+                String[] tempElem = {cursor.getString(0),cursor.getString(1)};
                 dataList.add(tempElem);
             }
         }
